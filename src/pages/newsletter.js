6e5e56f03f261784dac10f52643cf894a34c9dd7
MiguelARam27 <garandster27@live.com>
2021-03-27 09:25:51 -0400

    form change

--- src/pages/newsletter.js
+++ src/pages/newsletter.js
@@ -7,7 +7,14 @@ const NewsLetter = () => {
         <div className="page-center">
           <h2>Get all my latest stories to your inbox</h2>
           <h4>I write to my friends every weeks</h4>
-          <form className="contact-form">
+          <form
+            className="contact-form"
+            name="news"
+            method="post"
+            netlify-honeypot="bot-field"
+            data-netlify="true"
+            action="/success"
+          >
             <input
               type="text"
               name="name"
